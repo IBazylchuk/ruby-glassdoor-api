527ab7f25a76d68fa2c02a564692b4a58d27da20
Adam Bedford <adam.bedford@live.com>
2017-01-09 16:51:48 -0800

    Add company search

--- lib/glassdoor/clients/company.rb
+++ lib/glassdoor/clients/company.rb
@@ -1,6 +1,15 @@
 module Glassdoor
   module Clients
     module Company
+      def self.search_by_title(title)
+        api = Glassdoor::Utils::Api.instance
+        hash  = api.gd_get(action: 'employers', q: title)
+
+        hash['employers'].map do |company|
+          Models::Company.new(company)
+        end
+      end
+
       def self.find_by_title(title)
         api = Glassdoor::Utils::Api.instance
         hash  = api.gd_get(action: 'employers', q: title)
